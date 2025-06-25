bdd5fe4bc46e5e53aab398a89095826aa2645633
Sanghyeon Lee <cpd4268@naver.com>
2025-06-26 03:56:13 +0900

    [업데이트] service-worker.js

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,14 +1,36 @@
-// MV3 service-worker.js (모듈)
+'use strict'
 import {ACTIONS} from './constant/constant.js';
+
+
+chrome.declarativeNetRequest.onRuleMatchedDebug.addListener((e) => {
+    const msg = `Navigation blocked to ${e.request.url} on tab ${e.request.tabId}.`;
+    console.log(msg);
+});
 
 console.log('Service worker started.');
 
+
+let TOTAL_BLOCKED_COUNT = 0;
+
+chrome.declarativeNetRequest.onRuleMatchedDebug.addListener((info) => {
+    const {request, rule} = info;
+
+    // 여기서 request 사용 가능
+    if (request.documentLifecycle === 'prerender') {
+        return; // prerender 요청은 무시
+    }
+    console.log('🔍 Rule matched:', info);
+    TOTAL_BLOCKED_COUNT++;
+    chrome.storage.local.set({TOTAL_BLOCKED_COUNT});
+});
+
+
 const COSMETIC_JSON_URL = chrome.runtime.getURL('ruleset/cosmeticList-selector.json');
 let COSMETIC_RULESET = [];
-let TOTAL_BLOCKED_COUNT = 0;
 
 (async function loadRuleset() {
     try {
+        console.log(`ACTIONS.HIDE: ${ACTIONS.HIDE}`)
         const raw = await (await fetch(COSMETIC_JSON_URL)).json();
         COSMETIC_RULESET = raw.map(rule => ({
             selector: rule.selector,
@@ -21,23 +43,6 @@ let TOTAL_BLOCKED_COUNT = 0;
         COSMETIC_RULESET = [];
     }
 })();
-
-chrome.declarativeNetRequest.onRuleMatchedDebug.addListener((e) => {
-    const msg = `Navigation blocked to ${e.request.url} on tab ${e.request.tabId}.`;
-    console.log(msg);
-});
-
-chrome.declarativeNetRequest.onRuleMatchedDebug.addListener((info) => {
-    const {request, rule} = info;
-
-    // prerender 단계의 요청은 무시
-    if (request.documentLifecycle === 'prerender') {
-        return;
-    }
-    console.log('🔍 Rule matched:', info);
-    TOTAL_BLOCKED_COUNT++;
-    chrome.storage.local.set({TOTAL_BLOCKED_COUNT});
-});
 
 // 코스메틱 필터 룰셋 리스너 추가
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
